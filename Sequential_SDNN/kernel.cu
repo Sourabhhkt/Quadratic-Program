79aae151c139641d40762cab602f62f2457693c1
Sourabh Kumar Choudhary <66786226+Sourabhhkt@users.noreply.github.com>
2023-04-16 21:27:08 -0400

    Add files via upload

---
+++ Sequential_SDNN/kernel.cu
@@ -0,0 +1,35 @@
+__global__ void vecAddKernel(float* A, float* B, float* C, int n) {
+
+    // Calculate global thread index based on the block and thread indices ----
+
+    //INSERT KERNEL CODE HERE
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+
+    // Use global index to determine which elements to read, add, and write ---
+
+    //INSERT KERNEL CODE HERE
+    if (i < n)
+    {
+        C[i] = A[i]+B[i];
+    }
+}
+
+__global__ void sdnnIterationKernel(float* x_d, float* u_d, float*  ME_T_d, float* s_d, int row_num, int col_num) {
+
+    // Calculate global thread index based on the block and thread indices ----
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    if (i < row_num)
+    {
+        x_d[i] = s_d[i];
+        for (int j = 0; j < col_num; j++)
+        {
+            x_d[i] += (ME_T_d[i*col_num + j]*u_d[j]);
+            printf("i = %d, j = %d \n",i, j);
+            printf("ME_T_d[%d]: %f \n",i*col_num + j, ME_T_d[i*col_num + j]);
+            printf("x[%d]: %f \n",i, x_d[i]);
+        }
+    }
+    __syncthreads();
+    
+}
+
---
+++ Sequential_SDNN/support.cu
@@ -0,0 +1,176 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <float.h>
+
+#include "support.h"
+
+void verify(float *A, float *B, float *C, int n) {
+
+  const float relativeTolerance = 1e-6;
+
+  for(int i = 0; i < n; i++) {
+    float sum = A[i] + B[i];
+    float relativeError = (sum - C[i])/sum;
+    if (relativeError > relativeTolerance
+      || relativeError < -relativeTolerance) {
+      printf("TEST FAILED\n\n");
+      exit(0);
+    }
+  }
+  printf("TEST PASSED\n\n");
+
+}
+
+void startTime(Timer* timer) {
+    gettimeofday(&(timer->startTime), NULL);
+}
+
+void stopTime(Timer* timer) {
+    gettimeofday(&(timer->endTime), NULL);
+}
+
+float elapsedTime(Timer timer) {
+    return ((float) ((timer.endTime.tv_sec - timer.startTime.tv_sec) \
+                + (timer.endTime.tv_usec - timer.startTime.tv_usec)/1.0e6));
+}
+void print_1d_array(int _c, float* arr){
+  int j;
+  for (j = 0; j < _c; j++){
+      printf("%.5f ",arr[j]);
+  }
+  printf("\n");
+}
+
+void print_1d_array_usint(int _c, unsigned int* arr){
+  int j;
+  for (j = 0; j < _c; j++){
+      printf("%d ",arr[j]);
+  }
+  printf("\n");
+}
+
+void print_2d_array(int _r, int _c, float** arr){
+  int i, j;
+  for (i = 0; i < _r; i++){
+      for (j = 0; j < _c; j++){
+          printf("%.5f ", arr[i][j]);
+      }
+      printf("\n");
+  }
+}
+// def g_func(z, l, h):
+//     g = []
+//     for i in range(len(z)):
+//         if ((l[i] == -np.inf) and (h[i]==np.inf)):
+//             g.append(z[i])
+//         elif (l[i] == -np.inf):
+//             if (z[i]<= h[i]): g.append(z[i])
+//             else: g.append(h[i])
+//         elif (h[i]==np.inf):
+//             if (z[i]>= l[i]): g.append(z[i])
+//             else: g.append(l[i])
+//         else:
+//             if (z[i]< l[i]): g.append(l[i])
+//             elif ((l[i]<=z[i])and(z[i]<=h[i])): g.append(z[i])
+//             else: g.append(h[i])
+//     return np.array(g)
+
+float* g_function(int num_dim, float* z,  float* l,  float* h )
+{
+  float* ans = (float*) malloc( sizeof(float)*num_dim );
+  int i;
+  for (i = 0; i < num_dim; i++)
+  {
+    if ((l[i] == FLT_MIN) and (h[i]== FLT_MAX)) {
+      ans[i] = z[i];
+    }else if (l[i] == FLT_MIN)
+    {
+      if (z[i] <= h[i])
+      {
+        ans[i] = z[i];
+      }else{
+        ans[i] = h[i];
+      }
+    }else if (h[i] == FLT_MAX){
+      if (z[i] >= l[i])
+      {
+        ans[i] = z[i];
+      }else{
+        ans[i] = l[i];
+      }
+    }else{
+      if (z[i] < l[i])
+      {
+        ans[i] = l[i];
+      }else if ((l[i]<=z[i]) and (z[i]<=h[i])) {
+        ans[i] = z[i];
+      }else{
+        ans[i] = h[i];
+      }
+    }
+  }
+  return ans;
+}
+
+float* mat_mul_vec(int _r, int _c, float** _A,float* _vec)
+{
+  int _i,_j;
+  float* ans_arr = (float*)malloc(sizeof(float)*_r);;
+  for (_i = 0; _i < _r; _i++)
+  {
+    float _s = 0;
+    for (_j = 0; _j < _c; _j++){
+        _s = _s + _A[_i][_j]*_vec[_j];
+        // printf("%f ",_A[_i][_j]*_B[_j][_k]);
+        // printf("A[%d][%d] x B[%d][%d] = %.3f x %.3f = %f \n",_i,_j,_j,_k, _A[_i][_j],_B[_j][_k],_A[_i][_j]*_B[_j][_k]);
+    }
+    ans_arr[_i] = _s;   
+  }
+  // print_1d_array(_r, ans_arr);
+  return ans_arr;
+}
+
+float* vec_add_vec(int _r, float* _vec1,float* _vec2)
+{
+  int _j;
+  float* ans_vec = (float*)malloc(sizeof(float)*_r);
+  for (_j = 0; _j < _r; _j++){
+    ans_vec[_j] = _vec1[_j] + _vec2[_j];
+  }
+  // print_1d_array(_r, ans_vec);
+  return ans_vec;
+}
+
+float* scale_vec(int _r, float scaler, float* _vec)
+{
+  int _j;
+  float* ans_vec = (float*)malloc(sizeof(float)*_r);
+  for (_j = 0; _j < _r; _j++){
+    ans_vec[_j] = scaler*_vec[_j];
+  }
+  // print_1d_array(_r, ans_vec);
+  return ans_vec;
+}
+
+float* convert_2d_mat_to_1d_arr(int row_num, int col_num,float** ME_T){
+  float* mat_1d = (float*) malloc( sizeof(float)*row_num*col_num);
+  for (int i = 0; i < row_num; i++)
+  {
+    for (int j = 0; j < col_num; j++)
+    {
+      mat_1d[i*col_num + j] = ME_T[i][j];
+    }
+  }
+  return mat_1d;
+}
+
+float vec_l1_norm(int col_num, float* gradient){
+  float ans;
+  for (int j = 0; j < col_num; j++)
+  {
+    ans += abs(gradient[j]);
+  }
+  return ans;
+}
+
